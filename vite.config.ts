609149b6e320d1725a4385a5a12250a1206e2b3f
DmitriyGolub <heartnesnev@gmail.com>
2023-07-18 00:08:10 +0300

    feat: gh-pages update v1.0

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "",
   resolve: {
     alias: {
       gltf: `${path.resolve(__dirname, "./src/assets/gltf")}`,
