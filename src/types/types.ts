a68e90d729d83415e0c48ff92f0e3d09660c8bdf
DmitriyGolub <heartnesnev@gmail.com>
2023-07-18 14:57:41 +0300

    fix: animations Card, Hover feat: random idle

--- src/types/types.ts
+++
@@ -1,6 +0,0 @@
-import { GroupProps } from "@react-three/fiber/dist/declarations/src/three-types";
-import { Vector2 } from "three";
-
-export interface CustomGroupProps extends GroupProps {
-  uv?: Vector2;
-}
--- src/vite-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="vite/client" />
-
-import { JSX as DreiJSX } from "@react-three/drei";
-
-declare namespace JSX {
-  interface IntrinsicElements extends DreiJSX.IntrinsicElements {}
-}
